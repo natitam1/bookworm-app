32232a6d95fa00fb0aff218c38c06115256b6387
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-01 23:34:40 +0300

    chore: create a schema and a model

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI, () => {
-      console.log("Database connected");
-    });
+    const connection = await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database connected");
   } catch (error) {
     console.log("Error connecting to database", error);
     process.exit(1);
