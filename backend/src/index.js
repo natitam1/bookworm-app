be9e0eae74605a4c79c1725352b85865833145df
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-04 13:33:58 +0300

    chore: make it accept data in json

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,8 @@ import { connectDB } from "./lib/db.js";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => [
