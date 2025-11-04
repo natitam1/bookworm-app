320c2e4e5937ab221f6f40310f8d15a29d91c59a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-04 20:23:30 +0300

    chore: upload the image

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cloudinary from "../lib/cloudinary.js";
 
 const router = express.Router();
 
@@ -9,6 +10,19 @@ router.post("/", async (req, res) => {
       return res.status(500).json({ message: "Please provide all fields" });
     }
   } catch (error) {}
+});
+// Upload the image to cloudinary
+// Save to the database
+
+const uploadRepose = await cloudinary.uploader.upload(image);
+const imageUrl = uploadRepose.secure_url;
+
+const newBook = new Book({
+  title,
+  caption,
+  rating,
+  image: imageUrl,
+  user: req.user._id,
 });
 
 export default router;
