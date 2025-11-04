8e7c35014324c8a084f3a3f35b32e03683233fac
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-04 20:24:57 +0300

    fix: make the uploader function in the try block

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -9,20 +9,20 @@ router.post("/", async (req, res) => {
     if (!title || !caption || !rating || !image) {
       return res.status(500).json({ message: "Please provide all fields" });
     }
+    // Upload the image to cloudinary
+    // Save to the database
+
+    const uploadRepose = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadRepose.secure_url;
+
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      //   user: req.user._id,
+    });
   } catch (error) {}
-});
-// Upload the image to cloudinary
-// Save to the database
-
-const uploadRepose = await cloudinary.uploader.upload(image);
-const imageUrl = uploadRepose.secure_url;
-
-const newBook = new Book({
-  title,
-  caption,
-  rating,
-  image: imageUrl,
-  user: req.user._id,
 });
 
 export default router;
