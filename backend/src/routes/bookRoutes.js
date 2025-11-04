418804f690088f27773be7d1b0e845c26f0b98ef
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-04 20:15:44 +0300

    chore: get data from the body and verify it

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -2,4 +2,13 @@ import express from "express";
 
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+    if (!title || !caption || !rating || !image) {
+      return res.status(500).json({ message: "Please provide all fields" });
+    }
+  } catch (error) {}
+});
+
 export default router;
