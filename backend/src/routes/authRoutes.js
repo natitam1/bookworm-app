d09cedf09e3bde71618a02f2aff773f52b1e1b6a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-04 13:51:56 +0300

    chore: check for existing user

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,35 @@
 import express from "express";
+import User from "../models/user.js";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  res.send("Register");
+  try {
+    const { email, password, username } = req.body();
+    if (!email || !password || !username) {
+      return res.status(400).json({ message: "All fids are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
+    }
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username should be at least 3 characters long" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    const existingUsername = User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+  } catch (error) {}
 });
 router.post("/login", async (req, res) => {
   res.send("Login");
