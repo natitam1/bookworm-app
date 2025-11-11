b4cad2e56f53aa0f7f48fb52d022d0f2dfec500f
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-11 10:41:18 +0300

    chore: set up a store

---
+++ mobile/store/authStore.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  token: null,
+  isLoading: false,
+
+  register: async (username, email, password) => {
+    set({ isLoading: true });
+    try {
+      const response = await fetch("https://localhost:3000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+      const data = await response.json();
+      if (!response.ok) throw new Error(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}));
