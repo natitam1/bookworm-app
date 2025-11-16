f89452848a63367c3389fe046c068ef80e530e8e
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-16 17:17:56 +0300

    chore: set up handlers for the creating

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,5 +1,6 @@
 import { View, Text } from "react-native";
 import React, { useState } from "react";
+import { useRouter } from "expo-router";
 
 const create = () => {
   const [title, setTitle] = useState("");
@@ -8,6 +9,11 @@ const create = () => {
   const [image, setImage] = useState(null);
   const [imageBase64, setImageBase64] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const pickImage = async () => {};
+  const handleSubmit = async () => {};
 
   return (
     <View>
