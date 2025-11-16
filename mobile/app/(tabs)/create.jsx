c9b1247eebbaa708619c1d583e0a0c275cb51a5f
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-16 17:12:56 +0300

    chore: add state variables for the create screen

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,7 +1,14 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 const create = () => {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [image, setImage] = useState(null);
+  const [imageBase64, setImageBase64] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   return (
     <View>
       <Text>create</Text>
